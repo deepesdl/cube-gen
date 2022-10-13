00f5fbe8ba7d09f7801bca2c2353bef189d348f2
David Montero <dml.mont@gmail.com>
2022-10-13 16:37:46 +0200

    Modified CCI SM cube-gen and added metadata

--- ESDC/cube-gen/cci-sm-8d-0.25deg.py
+++ ESDC/cube-gen/cci-sm-8d-0.25deg.py
@@ -1,24 +1,17 @@
-from xcube.core.store import find_data_store_extensions
-from xcube.core.store import get_data_store_params_schema
-from xcube.core.store import new_data_store
-
-import shapely.geometry
-from IPython.display import JSON
-import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 import xarray as xr
+import glob
 
 print("Reading")
-store = new_data_store('cciodp')
+files = glob.glob("/net/scratch/dmontero/CCI/SM/*.zarr")
+files.sort()
 
-dataset = store.open_data(
-    'esacci.SOILMOISTURE.day.L3S.SSMV.multi-sensor.multi-platform.COMBINED.05-2.r1', 
-    variable_names=['sm'],
-    time_range=["1979-01-01","2019-12-31"]
-)
+dataset = [xr.open_zarr(file) for file in tqdm(files)]
+dataset = xr.concat(dataset,dim="time")
+dataset = dataset.chunk(dict(time=256))
 
-last_year = 2019
+last_year = 2020
 first_year = 1979
 
 years = np.arange(first_year,last_year + 1)
@@ -33,9 +26,7 @@ def resample_weekly(ds,year):
 print("Resampling in time")
 dataset_8d = [resample_weekly(dataset,year) for year in tqdm(years)]
 dataset_8d = xr.concat(dataset_8d,dim="time")
-
-print("Chunking")
-dataset_8d = dataset_8d.chunk(dict(time=256,lat=128,lon=128))
+dataset = dataset.chunk(dict(time=256))
 
 print("Saving")
 dataset_8d.to_zarr("/net/scratch/dmontero/CCI/cci-sm-8d-0.25deg-256x128x128.zarr")
