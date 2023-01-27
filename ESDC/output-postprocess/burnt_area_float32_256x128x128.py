8f2f565132c79976e3c452e8b4460e8fcf715fe4
davemlz <dml.mont@gmail.com>
2023-01-27 12:30:44 +0000

    Converted burnt_area from float64 to float32 (#11)

---
+++ ESDC/output-postprocess/burnt_area_float32_256x128x128.py
@@ -0,0 +1,15 @@
+from xcube.core.new import new_cube
+from xcube.core.store import find_data_store_extensions
+from xcube.core.store import get_data_store_params_schema
+from xcube.core.store import new_data_store
+from xcube.core.store import new_data_writer
+
+from xcube.core.dsio import open_dataset
+
+data_store = new_data_store("s3", root="deep-esdl-output")
+
+ds = data_store.open_data('esdc-8d-0.25deg-256x128x128-3.0.1.zarr')
+
+ds['burnt_area'] = ds.burnt_area.astype("float32")
+
+data_store.write_data(ds, 'esdc-8d-0.25deg-256x128x128-3.0.2.zarr')
