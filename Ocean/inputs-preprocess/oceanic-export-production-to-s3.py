759bdd99188f02e13e9f4272a84dc5c512d1cb8f
davemlz <dml.mont@gmail.com>
2023-02-08 22:09:01 +0000

    Added new files inputs-preprocess

---
+++ Ocean/inputs-preprocess/oceanic-export-production-to-s3.py
@@ -0,0 +1,25 @@
+from xcube.core.store import find_data_store_extensions
+from xcube.core.store import get_data_store_params_schema
+from xcube.core.store import new_data_store
+
+import xarray as xr
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+import os
+
+store_output = new_data_store("s3", root="deep-esdl-input")
+
+pathIn = f"~/data/ocean/source/monthly_global_oceanic_export_production/"
+pathIn = os.path.expanduser(pathIn)
+
+files = glob(f"{pathIn}/*.zarr")
+files.sort()
+
+print("Reading data...")
+ds = xr.concat([xr.open_zarr(file) for file in tqdm(files)],dim="time")
+
+ds = ds.chunk(dict(time=64))
+
+print("Writing data...")
+store_output.write_data(ds, "oceanic-export-production-1M-9km-64x256x256.zarr", replace=True)
