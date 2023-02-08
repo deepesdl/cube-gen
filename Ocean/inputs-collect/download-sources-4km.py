1e985172c00dd13f4446970206322c5eb195e955
davemlz <dml.mont@gmail.com>
2023-02-08 09:52:49 +0000

    Added inputs-collect

---
+++ Ocean/inputs-collect/download-sources-4km.py
@@ -0,0 +1,44 @@
+import json
+import requests
+import os
+import numpy as np
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+pathOut = "~/data/ocean/source/"
+pathOut = os.path.expanduser(pathOut)
+
+if not os.path.exists(pathOut):
+    os.makedirs(pathOut)
+
+with open('../cube4km.geojson', 'r') as f:
+    cube_specs = json.load(f)
+
+def list_files_nc(url, ext=''):
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    return [node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+
+def download_file(source):
+    print(f"Downloading {source}")
+    output_source_path = pathOut + "/" + source['name'].lower().replace(" ","_")
+    if not os.path.exists(output_source_path):
+        os.makedirs(output_source_path)
+    url = source['download_url']
+    years = np.arange(1997,2021)
+    files = []
+    print(f"Retrieving files...")
+    for year in tqdm(years):
+        r = requests.get(f"{url}{year}")
+        if r.status_code == 200:
+            files.extend(list_files_nc(f"{url}{year}/","download=1"))
+    files = np.unique(files).tolist()
+    print(f"Downloaidng files...")
+    for file in tqdm(files):
+        filename = file.replace("?download=1","").split("/")[-1]
+        output = output_source_path + "/" + filename
+        if not os.path.exists(output):
+            response = requests.get(file)        
+            open(output, "wb").write(response.content)
+
+[download_file(source) for source in cube_specs['properties']['sources']]
