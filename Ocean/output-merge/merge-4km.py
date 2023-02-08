cd024651e9aed6274e99284f2db9439d31e5a4be
davemlz <dml.mont@gmail.com>
2023-02-08 23:03:10 +0000

    Added output-merge

---
+++ Ocean/output-merge/merge-4km.py
@@ -0,0 +1,44 @@
+from xcube.core.store import find_data_store_extensions
+from xcube.core.store import get_data_store_params_schema
+from xcube.core.store import new_data_store
+
+import xarray as xr
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+import os
+from datetime import datetime
+
+store_output = new_data_store("s3", root="deep-esdl-output")
+
+pathIn = f"~/data/ocean/source/monthly_global_particulate_organic_carbon_(poc)/"
+pathIn = os.path.expanduser(pathIn)
+
+files = glob(f"{pathIn}/*.zarr")
+files.sort()
+
+print("Reading files...")
+ds = xr.concat([xr.open_zarr(file) for file in tqdm(files)],dim="time")
+
+ds = ds.chunk(dict(time=64))
+
+additional_attrs = {
+    "date_modified": str(datetime.now()),
+    "geospatial_lat_max": float(ds.lat.max().values),
+    "geospatial_lat_min": float(ds.lat.min().values),
+    "geospatial_lat_resolution": abs(float(
+        ds.lat[1] - ds.lat[0]
+    )),
+    "geospatial_lon_max": float(ds.lon.max().values),
+    "geospatial_lon_min": float(ds.lon.min().values),
+    "geospatial_lon_resolution": abs(float(
+        ds.lon[1] - ds.lon[0]
+    )),
+    "time_coverage_start": str(ds.time[0].values),
+    "time_coverage_end": str(ds.time[-1].values),
+}
+
+ds.attrs = additional_attrs
+
+print("Writing cube...")
+store_output.write_data(ds, "ocean-1M-4km-64x256x256-1.0.0.zarr", replace=True)
---
+++ Ocean/output-merge/merge-9km.py
@@ -0,0 +1,52 @@
+from xcube.core.store import find_data_store_extensions
+from xcube.core.store import get_data_store_params_schema
+from xcube.core.store import new_data_store
+
+import xarray as xr
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+import os
+from datetime import datetime
+
+store = new_data_store("s3", root="deep-esdl-input")
+store_output = new_data_store("s3", root="deep-esdl-output")
+
+print("Reading files...")
+datasets = [
+    'phytoplankton-primary-production-1M-9km-64x256x256.zarr',
+    'phytoplankton-carbon-1M-9km-64x256x256.zarr',
+    'oceanic-export-production-1M-9km-64x256x256.zarr'
+]
+
+das = [store.open_data(dataset) for dataset in datasets]
+
+for i in [1,2]:
+    das[i]["lat"] = das[0].lat
+    das[i]["lon"] = das[0].lon
+
+print("Merging datasets...")
+ds = xr.merge(das)
+
+ds = ds.chunk(dict(time=64))
+
+additional_attrs = {
+    "date_modified": str(datetime.now()),
+    "geospatial_lat_max": float(ds.lat.max().values),
+    "geospatial_lat_min": float(ds.lat.min().values),
+    "geospatial_lat_resolution": abs(float(
+        ds.lat[1] - ds.lat[0]
+    )),
+    "geospatial_lon_max": float(ds.lon.max().values),
+    "geospatial_lon_min": float(ds.lon.min().values),
+    "geospatial_lon_resolution": abs(float(
+        ds.lon[1] - ds.lon[0]
+    )),
+    "time_coverage_start": str(ds.time[0].values),
+    "time_coverage_end": str(ds.time[-1].values),
+}
+
+ds.attrs = additional_attrs
+
+print("Writing cube...")
+store_output.write_data(ds, "ocean-1M-9km-64x256x256-1.0.0.zarr", replace=True)
