4f6794fb036f55af35c169c6ddc01c8fd63aa2f5
davemlz <dml.mont@gmail.com>
2023-03-22 20:19:14 +0000

    FEAT: Added code to preprocess DOC 9km

---
+++ Ocean/inputs-preprocess/dissolved-organic-carbon-9km-to-s3.py
@@ -0,0 +1,32 @@
+from xcube.core.store import find_data_store_extensions
+from xcube.core.store import get_data_store_params_schema
+from xcube.core.store import new_data_store
+
+import xarray as xr
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+import os
+
+store_output = new_data_store("s3", root="deep-esdl-input")
+
+pathIn = f"~/data/ocean/source/dissolved_organic_carbon/"
+pathIn = os.path.expanduser(pathIn)
+
+files = glob(f"{pathIn}/*.zarr")
+files.sort()
+
+print("Reading data...")
+ds = xr.concat([xr.open_zarr(file) for file in tqdm(files)],dim="time")
+
+variables = list(ds.variables)
+for dim in ["lat","lon","time"]:
+    variables.remove(dim)
+
+for variable in variables:
+    del ds[variable].encoding['chunks']
+
+ds = ds.chunk(dict(time=64,lat=256,lon=256))
+
+print("Writing data...")
+store_output.write_data(ds, "dissolved-organic-carbon-1M-9km-64x256x256.zarr", replace=True)
