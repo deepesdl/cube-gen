239a1001cade4357d8f7ba457a7fcd0d54422546
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2024-11-20 09:40:56 +0100

    Made changes to Recipe to generate daily aggregates and merge into final cube

--- hydrology/README.md
+++ hydrology/README.md
@@ -10,13 +10,13 @@ The cubes generation process is divided in four phases:
 
 ### 1. Downloading required raw datasets
 
-Datasets were downloaded from the `4dmed_data.eodchosting.eu` sftp host. The following folders were downladed recursively using the sftp protocol:
+Datasets were downloaded from the `4dmed_data.eodchosting.eu` sftp host. The following folders were downloaded recursively using the sftp protocol:
 
 > **Note**
 >
 > Users must provide their own credentials to access the sftp host.
 
-- `/4dmed_data/precipitation_GPM_CPC_SM2A`
+- `/4dmed_data/CNR_products/precipitation_GPM_CPC_SM2RAIN-ASCAT`
 - `/4dmed_data/TUWien_RT1_SM`
 - `/4dmed_data/GLEAM_openloop_V1.1`
 - `/4dmed_data/SWE/SWE_CPC_GPM_ERA5downT_RadGhent_filter5mm`
@@ -25,7 +25,7 @@ Datasets were downloaded into a folder named `data/hydrology/source` in the user
 
 > **Note**
 >
-> The path `data/hydrology/source` should be creater by the user and the folders from the host should be downloaded here.
+> The path `data/hydrology/source` should be created by the user and the folders from the host should be downloaded here.
 
 The Snow Water Equivalent (SWE) product is zipped and has to be unzipped by running the following command:
 
@@ -59,13 +59,14 @@ python inputs-preprocess/swe-to-s3.py # Upload full cube to the s3 bucket
 
 ### 3. Merging all datasets into a single cube
 
-All `.zarr` cubes are loaded and merged by their coordinates and timesteps into a single `.zarr` cube.
+The `.zarr` cubes are loaded per region, aggregated to a daily temporal 
+resolution and merged by their coordinates and timesteps into a single `.zarr` cube.
 
 **If you wish to execute the below scripts please ensure to adjust the `root` parameter in the scripts to point to a s3 bucket which you have read-write access to.**
 
 ```
 # Merge all datasets
-python output-merge/merge.py
+python output-merge/daily_aggregates_and_merge.py
 ```
 
 ### 4. Postprocessing
@@ -77,4 +78,17 @@ A patch of metadata is added to the final cubes using `xcube patch` by running:
 output-postprocess/patch.sh
 ```
 
+If you wish to merge all subcubes into one, please execute the following:
+
+```
+# Merge all subcubes into a single cube
+python output-postprocess/merge-all-regions-into-one-cube.py
+```
+
+Update metadata of the final cube, containing all subregions:
+
+```
+# Update metadata of final cube:
+xcube patch s3://deep-esdl-output/hydrology-1D-0.009deg-1x1102x966-1.0.0.levels --metadata patch-hydrology-1D-0.009deg-1x1102x2415.yaml -v
+```
 **If you wish to execute the above script please ensure to adjust the s3 bucket path to point to the bucket which you have read-write access to.**
--- hydrology/output-merge/daily_aggregates_and_merge.py
+++ hydrology/output-merge/daily_aggregates_and_merge.py
@@ -14,18 +14,28 @@ store_output = new_data_store("s3", root="deep-esdl-output")
 
 sites = [(f"0{x}")[-2:] for x in np.arange(1,29)]
 
-def chunk_and_create_encoding_dict(ds, time_chunking, lat_chunking, lon_chunking, quiet=True):
+def chunk_and_create_encoding_dict(ds,
+                                   time_chunking,
+                                   lat_chunking,
+                                   lon_chunking,
+                                   quiet=True):
     
     for var_name in ds.data_vars:
         if var_name != "crs":
             if not quiet:
                 print(var_name)
-            ds[var_name] = ds[var_name].chunk({'time': time_chunking, 'lat': lat_chunking, 'lon': lon_chunking})
+            ds[var_name] = ds[var_name].chunk({'time': time_chunking,
+                                               'lat': lat_chunking,
+                                               'lon': lon_chunking})
             
     encoding_dict = dict()
     
     coords_encoding = {k: dict(chunks=v.shape) for k, v in ds.coords.items()}
-    vars_encoding = {k: dict(chunks=(time_chunking, lat_chunking, lon_chunking), write_empty_chunks=False) for k, v in ds.data_vars.items() if k != "crs"} 
+    vars_encoding = {k: dict(chunks=(time_chunking,
+                                     lat_chunking,
+                                     lon_chunking),
+                             dtype=np.dtype('float32'),
+                             write_empty_chunks=False) for k, v in ds.data_vars.items() if k != "crs"}
     
     encoding_dict.update(coords_encoding)
     encoding_dict.update(vars_encoding)
@@ -61,9 +71,14 @@ def save_site(site):
 
     lat_chunking = ds.lat.shape[0]
     lon_chunking = ds.lon.shape[0]
-    time_chunking = 64
-        
-    ds, encoding_dict = chunk_and_create_encoding_dict(ds, time_chunking, lat_chunking, lon_chunking, True)
+    time_chunking = 1
+
+    ds = ds.resample(time='1D').mean(skipna=True, keep_attrs=True)
+    ds, encoding_dict = chunk_and_create_encoding_dict(ds,
+                                                       time_chunking,
+                                                       lat_chunking,
+                                                       lon_chunking,
+                                                       True)
 
     additional_attrs = {
         "date_modified": str(datetime.now()),
@@ -85,7 +100,7 @@ def save_site(site):
     
     store_output.write_data(
         ds,
-        f"hydrology-S{site}-0.009deg-{time_chunking}x{lat_chunking}x{lon_chunking}-1.0.0.zarr",
+        f"hydrology-S{site}-0.009deg-1D-{time_chunking}x{lat_chunking}x{lon_chunking}-1.0.0.zarr",
         replace=True,
         encoding=encoding_dict
     )
